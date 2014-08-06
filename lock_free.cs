a023f5909e0887ea98ea8d22febc01088f155ead
Mike Filimonov <mfilimonov@gmail.com>
2014-08-06 14:27:23 +0400

    A simple performance test: Interlocked vs lock
    Using Interlocked.CompareExchange is not always as fast as you think it
    should be - sometimes a simple lock will be better.

---
+++ lock_free.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace lock_free
+{
+    class PerformanceTest
+    {
+        static void Main(string[] args)
+        {
+            var watch = new Stopwatch();
+            IterateWithCompareAndSwap(watch);
+            watch.Reset();
+            IterateWithLock(watch);
+            watch.Reset();
+            IterateWithPreRead(watch);
+        }
+
+        private static void ReportElapsedTime(String method, Stopwatch watch)
+        {
+            Console.WriteLine("{0} - Timewatch.Elapsed.Ticks: {1}", method, watch.Elapsed.Ticks);
+        }
+
+        private static void IterateWithCompareAndSwap(Stopwatch watch)
+        {
+            Int32[] array = new Int32[Int16.MaxValue];
+            watch.Start();
+            for (var i = 0; i < array.Length; ++i)
+            {
+                Interlocked.CompareExchange(ref array[i], 1, 1);
+            }
+            watch.Stop();
+            ReportElapsedTime("IterateWithCompareAndSwap", watch);
+        }
+
+        private static void IterateWithPreRead(Stopwatch watch)
+        {
+            Int32[] array = new Int32[Int16.MaxValue];
+            watch.Start();
+            for (var i = 0; i < array.Length; ++i)
+            {
+                if (array[i] == 1)
+                    Interlocked.CompareExchange(ref array[i], 1, 1);
+            }
+            watch.Stop();
+            ReportElapsedTime("IterateWithPreRead", watch);
+        }
+
+        private static void IterateWithLock(Stopwatch watch)
+        {
+            Int32[] array = new Int32[Int16.MaxValue];
+            watch.Start();
+            lock (array)
+            {
+                for (var i = 0; i < array.Length; ++i)
+                {
+                    if (array[i] == 1)
+                        array[i] = Int16.MaxValue;
+                }
+            }
+            watch.Stop();
+            ReportElapsedTime("IterateWithLock", watch);
+        }
+    }
+}
